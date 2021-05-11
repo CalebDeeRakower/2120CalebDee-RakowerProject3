32339447e6c857eff98b3a5d38d4283f84ef1f77
Caleb.DeeRakower@mail.citytech.cuny.edu <CalebDeeRakower>
2021-05-11 11:27:10 -0400

    Uploading project

---
+++ Caleb Dee-Rakower Shrink Spell Tutorial/Assets/ShrinkSpell.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkSpell : MonoBehaviour
+{
+  public GameObject theBison;
+  public bool spellWorking = false;
+  public GameObject MagicStars;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (spellWorking ==  true)
+    {
+      MagicStars.SetActive(true);
+
+    }
+    if (theBison.transform.localScale.y >=0.33)
+    {
+      theBison.transform.localScale -= new Vector3(0.001f,0.001f,0.001f);
+    }
+    else
+    {
+      spellWorking = false;
+    }
+    }
+}
